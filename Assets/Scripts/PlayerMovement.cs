7c44def4d782e16d8873536f86be83cf661e14cc
JayG1212 <jaybgunderson@gmail.com>
2024-06-12 00:51:20 -0500

    Added Jumping
    Got jumping and FPS movement mostly finished

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,11 +29,21 @@ public class PlayerMovement : MonoBehaviour
     public float groundDrag; // The drag we will add to our player's movement
     public LayerMask whatIsGround; // A layer we will apply to our planes
     bool grounded; // Tracks whether the player is grounded
+
+    // Jumping
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+    public KeyCode jumpKey = KeyCode.Space;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>(); // Gets the rigidbody we declared in our editor
         rb.freezeRotation = true; // Freezes our player's rotation, preventing us from falling over like earlier
+        readyToJump = true;
+        
     }
 
   
@@ -43,7 +53,8 @@ public class PlayerMovement : MonoBehaviour
         // Ground check
         grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //  Deterrmiens if player is grounded
         MyInput(); // Fetches keyboard input each frame
-
+        SpeedControl();
+       
         // Handles drag
         if (grounded)
         {
@@ -62,12 +73,56 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal"); // A and D & Left/Right arrow keys
         verticalInput = Input.GetAxisRaw("Vertical"); // W and S & Up/Down arrow keys
+
+        // Checks if space is pressed, then jump if it was
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
     }
 
     private void MovePlayer()
     {
         // Calculates movement direction
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput; // Allows us to walk in the dirrection we are looking
-        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); // Adds force to our movements
+
+        // On Ground
+        if (grounded) 
+        { 
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force); // Adds force to our movements  
+        }
+        else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force); // Adds force to our movements 
+        }
     }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        // Limits velocity
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+        
+        
+    }
+
+    private void Jump()
+    {
+        // Reset y velocity
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse); // Moves player up by jump force (Jumps)
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+    }
+
 }
