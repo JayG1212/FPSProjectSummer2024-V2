7fa34924b5609bc66bd815810648a9431cd0adea
JayG1212 <jaybgunderson@gmail.com>
2024-06-11 23:46:19 -0500

    Ground and Drag movement
    Fixed movement and added grounding to my player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,10 @@
 // Written by Jay Gunderson
 // 06/11/2024
+/*
+ Update 06/11/2024:
+    Added grounding and drag to my player's movement 
+    to make movement smoother
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,6 +23,12 @@ public class PlayerMovement : MonoBehaviour
     Vector3 moveDirection; 
     Rigidbody rb;
 
+
+    // Ground checker
+    public float playerHeight; // The height of the player
+    public float groundDrag; // The drag we will add to our player's movement
+    public LayerMask whatIsGround; // A layer we will apply to our planes
+    bool grounded; // Tracks whether the player is grounded
     // Start is called before the first frame update
     void Start()
     {
@@ -29,7 +40,19 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Ground check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround); //  Deterrmiens if player is grounded
         MyInput(); // Fetches keyboard input each frame
+
+        // Handles drag
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0;
+        }
     }
     private void FixedUpdate() // Better for physics related updates
     {
